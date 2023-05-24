84b685149d516f2e01fee6c260b9017a5a883188
micaela frigola <mica_frigola@icloud.com>
2023-05-24 15:46:48 +0200

    restaurant advanced ruby on rails

--- Gemfile
+++ Gemfile
@@ -27,6 +27,9 @@ gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
 
+gem "simple_form"
+
+gem 'faker', '~> 2.18', group: :development
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: [:index, :new, :create]
+  end
+  resources :reviews, only: [:show, :new, :update,]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
